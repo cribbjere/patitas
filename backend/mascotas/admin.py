5dda6b215db239d44d186fbe8d227baabf306aee
cribbjere <cribbjeremiasprog@gmail.com>
2026-06-20 16:39:21 -0300

    Modulo mascotas y especies completo

--- backend/mascotas/admin.py
+++ backend/mascotas/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Especie, Mascota
 
-# Register your models here.
+admin.site.register(Especie)
+admin.site.register(Mascota)
--- backend/mascotas/models.py
+++ backend/mascotas/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from clientes.models import Cliente
 
-# Create your models here.
+class Especie(models.Model):
+
+    nombre = models.CharField(
+        max_length=50,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.nombre
+    
+class Mascota(models.Model):
+
+    SEXOS = [
+        ('macho', 'Macho'),
+        ('hembra', 'Hembra'),
+    ]
+
+    ESTADOS = [
+        ('activo', 'Activo'),
+        ('fallecido', 'Fallecido'),
+    ]
+
+    nombre = models.CharField(max_length=50)
+
+    fecha_nacimiento = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    raza = models.CharField(max_length=50)
+
+    peso = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+
+    sexo = models.CharField(
+        max_length=10,
+        choices=SEXOS
+    )
+
+    estado = models.CharField(
+        max_length=15,
+        choices=ESTADOS,
+        default='activo'
+    )
+
+    cliente = models.ForeignKey(
+        Cliente,
+        on_delete=models.CASCADE,
+        related_name='mascotas'
+    )
+
+    especie = models.ForeignKey(
+        Especie,
+        on_delete=models.PROTECT,
+        related_name='mascotas'
+    )
+
+    def __str__(self):
+        return self.nombre
