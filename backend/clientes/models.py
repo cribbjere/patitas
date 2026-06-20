2eedc8787bf35231d9fec0b7b1797e56b9f711ed
cribbjere <cribbjeremiasprog@gmail.com>
2026-06-20 16:18:39 -0300

    Modulo clientes completo

--- backend/clientes/models.py
+++ backend/clientes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+
+    ESTADOS = [
+        ('activo', 'Activo'),
+        ('inactivo', 'Inactivo'),
+    ]
+
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    direccion = models.CharField(max_length=150)
+    estado = models.CharField(
+        max_length=10,
+        choices=ESTADOS,
+        default='activo'
+    )
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
