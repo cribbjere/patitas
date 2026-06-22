ecef5243302ac70c12e0b17a0e1888297494bd64
cribbjere <cribbjeremiasprog@gmail.com>
2026-06-22 04:45:58 -0300

    Implementacion de grupos y permisos por rol

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -2,4 +2,28 @@ from django.contrib import admin
 from .models import PerfilUsuario
 
 
-admin.site.register(PerfilUsuario)
+@admin.register(PerfilUsuario)
+class PerfilUsuarioAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'usuario',
+        'rol',
+        'estado',
+        'fecha_creacion'
+    )
+
+    list_filter = (
+        'rol',
+        'estado'
+    )
+
+    search_fields = (
+        'usuario__username',
+        'usuario__email',
+        'usuario__first_name',
+        'usuario__last_name'
+    )
+
+    readonly_fields = (
+        'fecha_creacion',
+    )
