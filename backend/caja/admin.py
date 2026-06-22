83bf240656993d869a8974e5fdf695c08d622fe5
cribbjere <cribbjeremiasprog@gmail.com>
2026-06-22 01:28:07 -0300

    Dashboard financiero de caja

--- backend/caja/admin.py
+++ backend/caja/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.db.models import Sum
+
 from .models import MovimientoCaja
 
 
@@ -19,3 +21,31 @@ class MovimientoCajaAdmin(admin.ModelAdmin):
         'motivo',
         'fecha'
     )
+
+    change_list_template = "admin/caja/movimientocaja/change_list.html"
+
+    def changelist_view(self, request, extra_context=None):
+
+        ingresos = MovimientoCaja.objects.filter(
+            tipo_movimiento='ingreso'
+        ).aggregate(
+            total=Sum('monto')
+        )['total'] or 0
+
+        egresos = MovimientoCaja.objects.filter(
+            tipo_movimiento='egreso'
+        ).aggregate(
+            total=Sum('monto')
+        )['total'] or 0
+
+        saldo = ingresos - egresos
+
+        extra_context = extra_context or {}
+        extra_context['ingresos'] = ingresos
+        extra_context['egresos'] = egresos
+        extra_context['saldo'] = saldo
+
+        return super().changelist_view(
+            request,
+            extra_context=extra_context
+        )
