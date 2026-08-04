912240d0f7826b37cf52c0f32a09f7c4ffce289f
antonellapaolin26 <antonellapaolin26@gmail.com>
2026-08-04 10:16:49 -0300

    Modificaciones en Backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
 
+const URL_API =
+  import.meta.env.VITE_API_URL
+  || 'http://127.0.0.1:8000/api'
+
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: URL_API,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
